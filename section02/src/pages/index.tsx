7c611f6c3521a68fd88845ff83464a45ec829465
Kimsu-in <tndls0258@naver.com>
2025-01-23 16:05:12 +0900

    :sparkles: 스타일링

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -1,3 +1,12 @@
+// CSS Module
+// 유니크한 값으로 변경되어서 다른 className과 겹치는 것을 방지
+import style from "./index.module.css";
+
 export default function Home() {
-  return <h1>인덱스</h1>;
+  return (
+    <>
+      <h1 className={style.h1}>인덱스</h1>
+      <h2 className={style.h2}>H2</h2>
+    </>
+  );
 }
