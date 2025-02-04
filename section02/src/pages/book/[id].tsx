4395b4b4e34a5d1f6261a6cefd9258d2ca28c071
Kimsu-in <tndls0258@naver.com>
2025-02-04 17:35:56 +0900

    :sparkles: 폴백 옵션 설정

--- section02/src/pages/book/[id].tsx
+++ section02/src/pages/book/[id].tsx
@@ -1,6 +1,7 @@
 import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
 import style from "./[id].module.css";
 import fetchOneBook from "@/lib/fetch-one-boos";
+import { useRouter } from "next/router";
 
 export const getStaticPaths = () => {
   // 동적경로 미리 설정해두어야 빌드해둠
@@ -10,13 +11,19 @@ export const getStaticPaths = () => {
       { params: { id: "2" } },
       { params: { id: "3" } },
     ],
-    fallback: false, // 대비책, 보험
+    fallback: true, // 대비책, 보험
   };
 };
 
 export const getStaticProps = async (context: GetStaticPropsContext) => {
   const id = context.params!.id; // [id].tsx는 param이 있어야만 들어올 수 있기 때문에 무조건 있다는 !를 사용해도 안전
   const book = await fetchOneBook(Number(id));
+
+  if (!book) {
+    return {
+      notFound: true, // 404 페이지로 리다이렉트
+    };
+  }
 
   return {
     props: { book },
@@ -26,6 +33,9 @@ export const getStaticProps = async (context: GetStaticPropsContext) => {
 export default function Page({
   book,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
+  const router = useRouter();
+
+  if (router.isFallback) return "로딩중입니다";
   if (!book) return "문제가 발생했습니다. 다시 시도하세요.";
 
   const { id, title, subTitle, description, author, publisher, coverImgUrl } =
